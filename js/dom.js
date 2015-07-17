2a80b1c02784c853b735a6153b5e6e78f10c6c74
benhassara <bhassara@gmail.com>
2015-07-17 16:22:55 -0600

    complete functionality for sum all, count selected

--- js/dom.js
+++ js/dom.js
@@ -9,3 +9,20 @@ Hints for the first problem:
     - takes the value returned by the `sum` function and displays it on the page
 
 */
+
+function sumAll() {
+  var valElems = document.getElementsByTagName('option');
+  var allVals = [];
+  for (var i = 0; i < valElems.length; i++) {
+    allVals.push(parseInt(valElems[i].value));
+  }
+  return sum(allVals);
+}
+
+var ansdiv = document.getElementById('ans');
+
+var btnSumAll = document.getElementById('sum-all');
+btnSumAll.setAttribute('onclick', "document.getElementById('ans').innerHTML=sumAll()");
+
+var btnCountSel = document.getElementById('count-selected');
+btnCountSel.setAttribute('onclick', "ansdiv.innerHTML = countSelected(document.getElementsByTagName('option'))");
--- js/logic.js
+++ js/logic.js
@@ -10,3 +10,19 @@ Hints for the first problem:
 NOTE: use a `for` loop to make this happen.
 
 */
+function sum(options) {
+  var sum = 0;
+  for (var i = 0; i < options.length; i++) {
+    sum += options[i];
+  }
+  return sum;
+}
+
+function countSelected(elems) {
+  var count = 0;
+  for (var i = 0; i < elems.length; i++) {
+    if (elems[i].selected === true)
+      count++;
+  }
+  return count;
+}
